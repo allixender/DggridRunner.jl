7e9c111fc4ed458c18538417bf2ffa451cd4f5b2
Alexander Kmoch <alexander.kmoch@ut.ee>
2026-02-27 16:52:21 +0200

    test ci then

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using DggridRunner
 using Test
 
 @testset "DggridRunner.jl" begin
-    # Write your tests here.
+    @testset "DggridParams" begin
+        include("test_dggrid_params.jl")
+    end
+    @testset "Point Output" begin
+        include("test_point_output.jl")
+    end
+    # @testset "Runner Lib" begin
+    #     # NOTE: This test is currently disabled due to a dynamic library loading issue with DGGRID7_jll.
+    #     # It can be run locally using the test/run_with_env.sh script.
+    #     # include("test_dggrid_runner_lib.jl")
+    # end
 end
